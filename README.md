65b182379eee4edfe1df0fafc19313abac8e6eaa
L0laapk3 <L0laapk3@gmail.com>
2018-01-05 23:47:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Plays armorgames games in fullscreen.
+
+requires tampermonkey(chrome) or greasemonkey(firefox).
+
+Install link: https://rawgit.com/L0laapk3/Fullscreen-ArmorGames/master/fullscreen.user.js
---
+++ fullscreen.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Fullscreen ArmorGames
+// @namespace    https://github.com/L0laapk3/Fullscreen-ArmorGames
+// @downloadURL  https://rawgit.com/L0laapk3/Fullscreen-ArmorGames/master/fullscreen.user.js
+// @version      1
+// @author       L0laapk3
+// @match        *://armorgames.com/play/*
+// ==/UserScript==
+
+
+$("#gamefileEmbed").css({height: "100vh", width: 100 * $("#gamefile").width() / $("#gamefile").height() + "vh", marginLeft: "calc(50vw - " + (50 * $("#gamefile").width() / $("#gamefile").height()) + "vh)"});
+$("#gamearea").css({height: "100vh", width: "100vw", position: "absolute", left: 0, top: 0, background: "black", zIndex: 5}).children("a").remove();
+$("#ag3-header").css({zIndex: 0});
+$("#gamefilearea").css({marginLeft: 0});
+$("body").css({overflowX: "hidden"});
+
+document.styleSheets[0].addRule("::-webkit-scrollbar", "visibility: hidden;");
